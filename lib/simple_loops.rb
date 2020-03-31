445098b2041202df133683598ea551b045604be1
Mathew Wheatley <mathew.p.wheatley@gmail.com>
2020-03-31 18:03:55 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, n)
+  n.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  array.length.times do |i|
+    puts array[i]
+  end
+end
+
+def return_string_array(array)
+  array.length.times do |i|
+    array[i] = array[i].to_s
+  end
+  return array
+end
